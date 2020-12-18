ef267978f65b4d060d651954bc0eceee6d218dba
Ali Eldamiry <alieldamiry@gmail.com>
2020-12-18 08:27:55 -0800

    finished translating navlinks

--- src/i18next.js
+++ src/i18next.js
@@ -6,6 +6,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
+const languages = ['en', 'ar'];
 
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -20,8 +21,8 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    debug: true,
-
+    debug: false,
+    whitelist: languages,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import RTL from './RTL';
+import './i18next';
 
 ReactDOM.render(
   <React.StrictMode>
     <RTL>
-      <App />
+      <Suspense fallback={<div>Loading....</div>}>
+        <App />
+      </Suspense>
     </RTL>
   </React.StrictMode>,
 
