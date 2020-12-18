aa021177b854c437e12ccd83105a7e60a77b8b1c
alieldamiry <alieldamiry@gmail.com>
2020-12-18 14:22:47 -0800

    Revert "preparing the project for multi direction layout"
    This reverts commit 4d7b37a7d4ccb810b99870e20b2ffa539e5b22ae.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { ThemeProvider } from '@material-ui/core';
-import theme from './theme';
 import './App.css';
 import NavBar from './components/Appbar';
 import Footer from './components/Sections/Footer/Footer';
@@ -8,11 +6,9 @@ import Home from './pages/Home';
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <NavBar />
-        <Home />
-        <Footer />
-      </ThemeProvider>
+      <NavBar />
+      <Home />
+      <Footer />
     </>
   );
 }
