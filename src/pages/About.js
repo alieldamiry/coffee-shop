478f0c496f1357c5a1f6832c065abf4ea596c7b5
Ali <alieldamiry@gmail.com>
2020-12-22 12:55:05 +0200

    finished about and services pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,20 @@
+import Achievements from '../components/Sections/Achievements/Achievements';
+import Hero from '../components/Sections/Hero/Hero';
+import Inner from '../components/Sections/Hero/Inner';
+import Menu from '../components/Sections/Menu/Menu';
+import Story from '../components/Sections/Story';
+import Testimonies from '../components/Sections/Testimonies/Testimonies';
+
 const About = () => (
-  <div>
-    About us page
-  </div>
+  <>
+    <Hero>
+      <Inner currentPage="About" />
+    </Hero>
+    <Story />
+    <Testimonies />
+    <Menu />
+    <Achievements />
+  </>
 );
 
 export default About;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,14 @@
-const Services = () => (
-  <div>
-    Services page
-  </div>
+import Hero from '../components/Sections/Hero/Hero';
+import Inner from '../components/Sections/Hero/Inner';
+import Services from '../components/Sections/Services/Services';
+
+const ServicesPage = () => (
+  <>
+    <Hero>
+      <Inner currentPage="Services" />
+    </Hero>
+    <Services />
+  </>
 );
 
-export default Services;
+export default ServicesPage;
