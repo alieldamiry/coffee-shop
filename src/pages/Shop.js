95ce3d86df1f2385ce8efbcce0382beffe4f1a67
alieldamiry <alieldamiry@gmail.com>
2021-01-10 13:52:47 +0200

    finished shop page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -6,12 +6,19 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import * as actions from '../store/actions/products';
 import Product from '../components/Product';
+import Hero from '../components/Sections/Hero/Hero';
+import Inner from '../components/Sections/Hero/Inner';
 
 const useStyles = makeStyles(() => ({
   shop: {
     padding: '4rem 10%',
     color: '#fff',
     textAlign: 'center',
+  },
+  bottomMargin: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '3rem',
   },
 }));
 
@@ -36,7 +43,7 @@ const Shop = () => {
     } else {
       setFilteredProducts(products);
     }
-  }, [filters]);
+  }, [filters, products]);
 
   const handleChange = (event) => {
     setFilters({
@@ -46,45 +53,50 @@ const Shop = () => {
   };
 
   return (
-    <section className={classes.shop}>
-      <FormGroup row>
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.coffee}
-              onChange={handleChange}
-              name="coffee"
-              color="primary"
-            />
+    <>
+      <Hero>
+        <Inner currentPage="Order Online" />
+      </Hero>
+      <section className={classes.shop}>
+        <FormGroup className={classes.bottomMargin} row>
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.coffee}
+                onChange={handleChange}
+                name="coffee"
+                color="primary"
+              />
             )}
-          label="Coffee"
-        />
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.drinks}
-              onChange={handleChange}
-              name="drinks"
-              color="primary"
-            />
+            label="Coffee"
+          />
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.drinks}
+                onChange={handleChange}
+                name="drinks"
+                color="primary"
+              />
             )}
-          label="Drinks"
-        />
-      </FormGroup>
-      <Grid container spacing={5}>
-        {filteredProducts ? filteredProducts.map((p) => (
-          <Grid item xs={12} sm={6} md={3} key={p.name}>
-            <Product
-              id={p.id}
-              name={p.name}
-              image={p.image}
-              description={p.description}
-              price={p.price}
-            />
-          </Grid>
-        )) : null}
-      </Grid>
-    </section>
+            label="Drinks"
+          />
+        </FormGroup>
+        <Grid container spacing={5}>
+          {filteredProducts ? filteredProducts.map((p) => (
+            <Grid item xs={12} sm={6} md={3} key={p.name}>
+              <Product
+                id={p.id}
+                name={p.name}
+                image={p.image}
+                description={p.description}
+                price={p.price}
+              />
+            </Grid>
+          )) : null}
+        </Grid>
+      </section>
+    </>
   );
 };
 
