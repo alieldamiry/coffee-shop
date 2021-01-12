aaec357fbb234dc7aaac22912a76c6bfdd13fdf5
alieldamiry <alieldamiry@gmail.com>
2021-01-13 01:39:48 +0200

    finished cart page ui

---
+++ src/components/CartProduct.js
@@ -0,0 +1,92 @@
+import { makeStyles } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveRoundedIcon from '@material-ui/icons/RemoveRounded';
+import AddRoundedIcon from '@material-ui/icons/AddRounded';
+import DeleteIcon from '@material-ui/icons/Delete';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import red from '@material-ui/core/colors/red';
+
+const useStyles = makeStyles((theme) => ({
+  productImg: {
+    width: 100,
+    margin: 'auto',
+    '& img': {
+      width: '100%',
+    },
+  },
+  iconButton: {
+    border: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+    cursor: 'pointer',
+    transition: '0.3s',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+    },
+    '&:hover svg': {
+      color: '#fff',
+    },
+  },
+  quantity: {
+    margin: '1rem',
+  },
+  icon: {
+    fontSize: '1.2rem',
+  },
+  deleteIcon: {
+    color: red[900],
+  },
+  counterGroup: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+const CartProduct = ({ image, name, price, quantity }) => {
+  const classes = useStyles();
+  return (
+    <TableRow key={name}>
+      <TableCell component="th" scope="row">
+        <IconButton>
+          <DeleteIcon title="tss" className={classes.deleteIcon} />
+        </IconButton>
+      </TableCell>
+      <TableCell align="center">
+        <div className={classes.productImg}>
+          <img
+            src={image}
+            alt={name}
+          />
+        </div>
+      </TableCell>
+      <TableCell align="center">
+        {name}
+      </TableCell>
+      <TableCell align="center">
+        $
+        {price.toFixed(2)}
+      </TableCell>
+      <TableCell align="center">
+        <div className={classes.counterGroup}>
+          <IconButton className={classes.iconButton}>
+            <RemoveRoundedIcon color="primary" className={classes.icon} />
+          </IconButton>
+          <div className={classes.quantity}>{quantity}</div>
+          <IconButton className={classes.iconButton}>
+            <AddRoundedIcon color="primary" className={classes.icon} />
+          </IconButton>
+        </div>
+      </TableCell>
+      <TableCell align="center">
+        $
+        {(quantity * price).toFixed(2)}
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default CartProduct;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,119 @@
-const Cart = () => (
-  <div>
-    Cart page
-  </div>
-);
+import { Button, Container, makeStyles } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { useSelector } from 'react-redux';
+import Inner from '../components/Sections/Hero/Inner';
+import Hero from '../components/Sections/Hero/Hero';
+import CartProduct from '../components/CartProduct';
 
+const useStyles = makeStyles((theme) => ({
+  cart: {
+    padding: '3rem 0',
+    color: '#ffffff',
+  },
+  table: {
+    minWidth: 650,
+    '& th, & td': {
+      fontSize: '1.1rem',
+    },
+  },
+  tableContainer: {
+    maxWidth: 1200,
+    marginBottom: '2rem',
+  },
+  tablHead: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  summaryRow: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  boxContainer: {
+    minWidth: 300,
+  },
+  totalsBox: {
+    border: '1px solid rgba(255, 255, 255, 0.1)',
+    padding: '1rem',
+  },
+  totalPrice: {
+    color: theme.palette.primary.light,
+  },
+  totalRow: {
+    display: 'flex',
+    padding: '1rem 0',
+    opacity: 0.7,
+    '& div': {
+      flex: 1,
+    },
+  },
+  borderTop: {
+    borderTop: '1px solid #fff',
+  },
+}));
+
+const Cart = () => {
+  const classes = useStyles();
+  const cartProducts = useSelector((state) => state.cart.cartProducts);
+
+  return (
+    <>
+      <Hero>
+        <Inner currentPage="Cart" />
+      </Hero>
+      <section className={classes.cart}>
+        <Container>
+          <TableContainer className={classes.tableContainer}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead className={classes.tablHead}>
+                <TableRow>
+                  <TableCell>&nbsp;</TableCell>
+                  <TableCell align="center">&nbsp;</TableCell>
+                  <TableCell align="center">Product&nbsp;</TableCell>
+                  <TableCell align="center">Price&nbsp;</TableCell>
+                  <TableCell align="center">Quantity&nbsp;</TableCell>
+                  <TableCell align="center">Total&nbsp;</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {cartProducts && cartProducts.map((p) => (
+                  <CartProduct
+                    key={p.id}
+                    image={p.image}
+                    name={p.name}
+                    price={p.price}
+                    quantity={p.quantity}
+                  />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className={classes.summaryRow}>
+            <div className={classes.boxContainer}>
+              <div className={classes.totalsBox}>
+                <h2>CART TOTALS</h2>
+                <div className={classes.totalRow}>
+                  <div>Subtotal: </div>
+                  <div>$20.60</div>
+                </div>
+                <div className={classes.totalRow}>
+                  <div>Delivery: </div>
+                  <div>$20.60</div>
+                </div>
+                <div className={[classes.totalRow, classes.borderTop].join(' ')}>
+                  <div>Total: </div>
+                  <div className={classes.totalPrice}>$20.60</div>
+                </div>
+              </div>
+              <Button color="primary" variant="contained" fullWidth>Checkout</Button>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
 export default Cart;
